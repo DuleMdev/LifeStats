9c4711d109adb1670580cc272bc3cc2c331f7b2f
Dusan Mitrovic <dusanmitrovic2001@gmail.com>
2023-05-03 15:02:19 +0200

    Assets integrated

--- Assets/Scripts/Models/Stat.cs
+++ Assets/Scripts/Models/Stat.cs
@@ -1,18 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Stat : MonoBehaviour
+public class Stat
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string name;
+    public string description;
 }
